3029588c95ea5f17f23cafe3e182dca0a28c8c2e
MirageM <imiragesz@gmail.com>
2023-10-07 23:09:01 -0400

    prev2 = prev2.next;

--- attempt1.java
+++ attempt1.java
@@ -10,6 +10,9 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        if(head.next == null){
+            return true;
+        }
         //reverse
         ListNode current = head;
         ListNode next = null;
@@ -20,12 +23,14 @@ class Solution {
             prev = current;
             current = next;
         }
+        ListNode prev2 = prev;
         ListNode current2 = head;
         while(current2 != null){
-            if(head.val != prev.val){
+            if(current2.val != prev2.val){
                 return false;
             }
-            current2 = current2.next;
+            current2 = current2.next; 
+            prev2 = prev2.next;
         }
         return true;
         
